25cbf07a9f16b004251c7b475471a7376b3900f3
Sil Westerveld <git@silwesterveld.com>
2018-10-26 20:05:47 +0200

    Implement function that totals up the value of all items in the cart

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -3,6 +3,7 @@ class ShoppingCart {
     this.items = []
   }
 
+  // Add an item to the cart
   addItem(itemName, quantity, price) {
     this.items.push({
       name: itemName,
@@ -11,12 +12,21 @@ class ShoppingCart {
     })
   }
 
+  // Get a list of items currently in the cart
   getItems() {
     return this.items
   }
 
+  // Empty the cart
   clear() {
     this.items = []
+  }
+
+  // Total up the value of all items currently in the cart
+  total() {
+    return this.getItems()
+      .map(item => item.quantity*item.pricePerUnit)
+      .reduce((acc,cur) => acc+cur, 0)
   }
 }
 
